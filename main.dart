c35d782cc797be4c8808461922ab4dee79c203a5
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-08-14 22:26:36 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,22 @@
-blah blah
+import 'package:flutter/material.dart';
+
+//The main fuction is the entrence point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp{
+      home:Scaffold{
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar{
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ), //AppBar
+        body: Center(
+          child: Image(
+            image:
+              NetworkImage('https://www.w3schools.cm/w3css/img_lights.jpg'),
+            ), // Image
+         ), // Center
+       ), // Scaffold
+     ), // MaterialApp
+   );
+}
